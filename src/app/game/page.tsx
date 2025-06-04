7b56d66cafdfcc1e2448efe8710f1d07dd05b904
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-04 18:49:08 +0200

    adding bg in AG page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,6 +1,8 @@
 const GamePage = () => {
     return(
+        <div className="bg-[url('/images/background/AG_bg.jpeg')] grayscale-50 bg-left-bottom min-h-screen ">
         <h1>About the Steam-Wreck Game</h1>
+        </div>
     )
 }
 

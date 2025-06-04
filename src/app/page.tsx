b043f8bbe81cbc7174910ba43b0313ce9ccb0d5c
andre84o <andre.torabpour@gmail.com>
2025-06-04 19:54:13 +0200

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <h1>MAIN BRANCH</h1>
-    
+    <div className="flex flex-col min-h-screen">
+      <div className="h-[70vh] bg-[url('/images/background/keyart.jpeg')] bg-cover bg-center"></div>
+      <div className="h-[30vh] bg-[url('/images/background/EnvironmentConceptGroundlevel.jpeg')] bg-cover bg-center"></div>
+    </div>
   );
 }
+//w-1/2 relative bg-[url('/images/background/keyart.jpeg')] bg-cover bg-right bg-center min-h-screen flex items-center justify-center px-4 sm:px-8 md:px-16

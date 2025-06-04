76542cfd8b973ab7e87216426906b149e3440668
Carl Åberg <caaberg99@gmail.com>
2025-06-04 16:24:41 +0200

    MVP (excluding array) for team-cards

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,6 +1,11 @@
+import PeopleCards from "@/features/components/AT-people-Cards/People-card"
+
 const TeamPage = () => {
     return (
+        <>
         <h1>About the team members in steam-wreck</h1>
+        <PeopleCards/>
+        </>
     )
 }
 

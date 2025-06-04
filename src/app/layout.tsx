8552900583159a052dd6be060439e8f7e81e0da3
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-06-04 11:12:10 +0200

    header component, layout bg with black top bar and gray navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Footer from "@/components/Footer";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Header/>
         <Footer />
       </body>
     </html>

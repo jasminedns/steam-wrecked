f5236952ff55a77cd1bf9fc4bbd62471e7cbd493
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-04 17:28:41 +0200

    giving exact link to the links in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 import { Pages } from "@/features/arrays/types"
-
+import Link from "next/link"
 const Footer = () => {
     return (
         <footer className="bg-gray-400 text-white">
-            <div className="uppercase text-xs m-4">
-                <p>Homepage</p>
+            <div className="uppercase text-xs m-4 flex flex-col">
+                <Link href="/">Homepage</Link>
                     {Pages.map((item:string,index:number) => (
-                        <p key={index} className="uppercase text-xs">{item.includes("the") ? `about  ${item}` : item}</p>
+                        <Link key={index} href={`/${item.replace(/^the /i, "")}`} className="uppercase text-xs">{item.includes("the") ? `about  ${item}` : item}</Link>
                     ))}
             </div>
         </footer>

681b61c19572cd63cb5d22cb1c14fdda4ce60890
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-04 11:42:31 +0200

    installing react-icon package and adding social logos

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,29 @@
+import { SiItchdotio } from "react-icons/si"
+import { FaXTwitter,FaInstagram,FaYoutube } from "react-icons/fa6";
+
 const Footer = () => {
     return (
-        <footer className="bg-gray-400 text-white">
-            <p>&copy; 2025</p>
+        <footer className="bg-gray-400 text-white flex justify-between p-2">
+            <div className="text-white">
+                <p>Links</p>
+                <p>Links</p>
+                <p>Links</p>
+                <p>Links</p>
+            </div>
+            <div className="flex mt-14 ">
+            <div className="bg-black rounded-3xl m-1">
+            <FaInstagram size={32} className="text-gray-600 m-1 p-1"/>
+            </div>
+            <div className="bg-black rounded-3xl m-1">
+      <FaXTwitter size={32} className="text-gray-600 m-1 p-1" />
+      </div>
+      <div className="bg-black rounded-3xl m-1">
+      <FaYoutube size={32}  className="text-gray-600 m-1 p-1" />
+      </div>
+      <div className="bg-black rounded-3xl m-1">
+      <SiItchdotio size={32}   className="text-gray-600 m-1 p-1" />
+      </div>
+      </div>
         </footer>
     )
 }

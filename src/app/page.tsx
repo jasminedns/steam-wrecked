c291249817eea146115cbb2e35b91e47b95941dd
Jasminedns <jasminedanese@icloud.com>
2025-06-05 17:13:13 +0200

    image row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="charter text-6xl font-bold">MAIN BRANCH</h1>
+      <div className="flex justify-evenly">
+        <Image src="/images/player/selected_player.png" alt="" height={300} width={200}/>
+        <Image src="/images/player/player1.png" alt="" height={300} width={200}/>
+        <Image src="/images/player/player4.png" alt="" height={300} width={200}/>
+      </div>
     </div>
   );
 }

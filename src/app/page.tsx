0a59b180524503057fe967b2a9c7f7b6db1ab85b
Mustafa Larhnimi <mustafa.larhnimi@gmail.com>
2025-06-05 09:47:50 +0200

    Update Charter font implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ export default function Home() {
   return (
     <div>
       <h1 className="charter text-6xl font-bold">MAIN BRANCH</h1>
-      <h2 className="charter text-3xl">Charter Font is gray-ish</h2>
-      <p className="charter text-lg">This uses Charter font</p>
-      <div className="charter text-sm">Small text</div>
     </div>
   );
 }

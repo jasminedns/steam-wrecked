996f1225db3453745ebeca0aed53c725f7fbac7c
andre84o <andre.torabpour@gmail.com>
2025-06-06 12:34:49 +0200

    footer fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="h-[70vh] bg-[url('/images/background/keyart.jpeg')] bg-cover bg-center"></div>
-       <div className="absolute bottom-30 left-0 w-full h-3 bg-gradient-to-b from-transparent to-white/30 backdrop-blur-sm z-10 pointer-events-none"></div>
-      <div className="h-[30vh] bg-[url('/images/background/EnvironmentConceptGroundlevel.jpeg')] bg-cover bg-top w-full"></div>
-    </div>
+    <h1>MAIN BRANCH</h1>
+    
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { FaXTwitter,FaInstagram,FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-400 bg-opacity-50 text-white flex justify-between p-2">
+        <footer className="relative bottom-0 bg-gray-400/50 text-white flex justify-between p-2]">
             <div className="text-white">
                 <p>links</p>
                 <p>links</p>

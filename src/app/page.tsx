d38f32dedac63f8e7bc2d098a9d383f1ea234fbe
andre84o <andre.torabpour@gmail.com>
2025-06-06 12:38:41 +0200

    HP fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="h-[70vh] bg-[url('/images/background/keyart.jpeg')] bg-cover bg-center"></div>
-       <div className="absolute bottom-30 left-0 w-full h-3 bg-gradient-to-b from-transparent to-white/30 backdrop-blur-sm z-10 pointer-events-none"></div>
-      <div className="h-[30vh] bg-[url('/images/background/EnvironmentConceptGroundlevel.jpeg')] bg-cover bg-top w-full"></div>
+      <div className="absolute top-[calc(76vh-0.60rem)] left-0 w-full h-4 bg-gradient-to-b from-transparent to-black/30 backdrop-blur-sm pointer-events-none z-20"></div>
+      <div className="relative -mb-26 h-[70vh] bg-[url('/images/background/EnvironmentConceptGroundlevel.jpeg')] bg-cover w-full"></div>
     </div>
+
   );
 }

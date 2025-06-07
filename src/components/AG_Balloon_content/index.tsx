9fe084fd3ecb77086f584e915eb8bc5966422ac4
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-07 20:58:36 +0200

    giving relative to parent

--- src/components/AG_Balloon_content/index.tsx
+++ src/components/AG_Balloon_content/index.tsx
@@ -3,17 +3,16 @@ import Image from "next/image";
 const AGBalloonDiv = () => {
     return (
         <>
-        <div className="mt-40 flex w-full justify-center">
-            <div className="bg-[#060A0A]/65  w-[250px] sm:w-[400px] md:w-[550px] lg:w-[800px] p-5 rounded-[45px] text-white 
-                absolute left-[15%] top-[80%] sm:left-[-10%] sm:top-[100%] md:left-[-30%] md:top-[50%] lg:left-[-35%] lg:top-[70%]">
+        <div className="mt-40relative flex w-full justify-center">
+          <div className="bg-[#060A0A]/65  w-[250px] sm:w-[400px] md:w-[550px] lg:w-[800px] p-5 rounded-[45px] text-white 
+                absolute top-[175%]">
                 <p className="mt-4 mx-4 sm:mx-16 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eum quisquam illum similique sunt. Deleniti, quae ab fuga ducimus distinctio laudantium tempora dolorem alias nihil rerum aspernatur, quibusdam quod architecto?
                 </p>      
             </div>
+            <Image src="/images/background/airship_concept_art.png" alt="airship" width={0} height={0} sizes="100vw"
+                className="w-[150px] sm:w-[250px] lg:w-[300px] h-auto absolute top-[100%] left-[0%] sm:left-[-30%] sm:top-[70%] md:left-[-68%] md:top-[60%] lg:left-[-65%] lg:top-[-5%]">
+            </Image>
         </div>
-        
-        <Image src="/images/background/airship_concept_art.png" alt="airship" width={0} height={0} sizes="100vw"
-           className="w-[150px] sm:w-[250px] lg:w-[300px] h-auto absolute top-[55%] left-[0%] sm:left-[-40%] sm:top-[40%] md:top-[-5%] md:left-[-60%] lg:left-[-65%] lg:top-[-5%]">
-        </Image>
         </>
     )
 }

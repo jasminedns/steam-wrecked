8254d733d95db50e4354db884a61f69aeb629287
Jasminedns <jasminedanese@icloud.com>
2025-06-07 18:09:17 +0200

    fixed errors for build failed

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,11 +3,8 @@ import PeopleCards from "@/features/components/AT-people-Cards/People-card"
 const TeamPage = () => {
     return (
         <>
-        <h1>About the team members in steam-wreck</h1>
-<<<<<<< HEAD
-=======
-        <PeopleCards/>
->>>>>>> dfd74ed82bb70b9d2f2dcc7a21d00bf64a84b7f9
+            <h1>About the team members in steam-wreck</h1>
+            <PeopleCards/>
         </>
     )
 }

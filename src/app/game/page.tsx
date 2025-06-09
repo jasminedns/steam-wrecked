70255672d8b0823b808ef5cf87bb458fca0b70c8
andre84o <andre.torabpour@gmail.com>
2025-06-09 14:58:48 +0200

    fixed conflict

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,13 +4,9 @@ import AGHeading from "@/components/AGHeading"
 const GamePage = () => {
     return(
         <>
-<<<<<<< HEAD
-        <div className="relative h-screen bg-[#2F5E72]">
-            <div className="absolute inset-0">
-=======
+
         <div className="bg-[#2F5E72]">
             <div className=" inset-0">
->>>>>>> 8b672f336e6e954dbe1f32cc7b3779d39ac5c65b
                 <div className="relative z-10 text-white flex flex-col items-center justify-center h-full ">
                     <img src="/images/background/AG_bg.JPEG" alt="Background" className="w-full h-full object-cover object-left-bottom opacity-20 mix-blend-multiply" />
                     <div className="absolute top-4">
@@ -23,16 +19,7 @@ const GamePage = () => {
                 images and video
             </div>
         </div>
-<<<<<<< HEAD
-        <div className="bg-[linear-gradient(75deg,rgba(23,109,130,1)_0%,rgba(130,100,75,1)_27%,rgba(32,113,135,1)_61%,rgba(16,84,94,1)_100%)] h-200 ">
-              images and video
-        </div>
-<<<<<<< HEAD
-=======
 
->>>>>>> Ticket-06/Create-Hp
-=======
->>>>>>> 8b672f336e6e954dbe1f32cc7b3779d39ac5c65b
         </>
     )
 }

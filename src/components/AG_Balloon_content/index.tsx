02fbc11fb98b65c5e77acf8ff959503e703e881a
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-10 23:22:33 +0200

    fixing width of the container

--- src/components/AG_Balloon_content/index.tsx
+++ src/components/AG_Balloon_content/index.tsx
@@ -4,13 +4,13 @@ const AGBalloonDiv = () => {
     return (
         <>
         <div className="mt-40 relative flex w-full justify-center">
-            <div className="bg-[#060A0A]/65  w-[300px] sm:w-[400px] md:w-[550px] lg:w-[800px] p-5 rounded-[45px] text-white 
+            <div className="bg-[#060A0A]/65  w-[350px] sm:w-[400px] md:w-[550px] lg:w-[800px] p-5 rounded-[45px] text-white 
                 absolute top-[-40px] lg:top-[-90px]">
                 <p className="mt-4 mx-4 sm:mx-16 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eum quisquam illum similique sunt. Deleniti, quae ab fuga ducimus distinctio laudantium tempora dolorem alias nihil rerum aspernatur, quibusdam quod architecto?
                 </p>      
             </div>
             <Image src="/images/background/airship_concept_art.png" alt="airship" width={0} height={0} sizes="100vw"
-                className="w-[150px] sm:w-[250px] md:w-[200px] lg:w-[300px] h-auto absolute left-[0%]  top-[-200px] sm:top-[-200px] sm:left-[-2%] md:top-[-200px] md:left-[-15%] lg:left-[-45%]  lg:top-[-250px]">
+                className="w-[150px] sm:w-[250px] md:w-[200px] lg:w-[300px] h-auto absolute left-[2%]  top-[-200px] sm:top-[-200px] sm:left-[-2%] md:top-[-200px] md:left-[-15%] lg:left-[-45%]  lg:top-[-250px]">
             </Image>
         </div>
         </>

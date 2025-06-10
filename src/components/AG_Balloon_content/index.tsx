e1687a216798d447dcd62b42d2a80821f9e0c0fa
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-10 22:49:04 +0200

    fixing airship alignment

--- src/components/AG_Balloon_content/index.tsx
+++ src/components/AG_Balloon_content/index.tsx
@@ -10,7 +10,7 @@ const AGBalloonDiv = () => {
                 </p>      
             </div>
             <Image src="/images/background/airship_concept_art.png" alt="airship" width={0} height={0} sizes="100vw"
-                className="w-[150px] sm:w-[250px] lg:w-[300px] h-auto absolute left-[-2%]  top-[-150px] sm:top-[-200px] sm:left-[-35%] md:top-[-220px] md:left-[-60%] lg:left-[-65%]  lg:top-[-300px]">
+                className="w-[150px] sm:w-[250px] md:w-[200px] lg:w-[300px] h-auto absolute left-[0%]  top-[-200px] sm:top-[-200px] sm:left-[-2%] md:top-[-200px] md:left-[-15%] lg:left-[-45%]  lg:top-[-250px]">
             </Image>
         </div>
         </>

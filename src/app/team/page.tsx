9775ede63e2c4370577660860224306ed7fdc3c7
Jasminedns <jasminedanese@icloud.com>
2025-06-10 15:09:10 +0200

    fixed errors in team page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,20 +3,8 @@ import PeopleCards from "@/features/components/AT-people-Cards/People-card"
 const TeamPage = () => {
     return (
         <>
-<<<<<<< HEAD
-        <h1>About the team members in steam-wreck</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        <PeopleCards/>
->>>>>>> dfd74ed82bb70b9d2f2dcc7a21d00bf64a84b7f9
-=======
-        <PeopleCards/>
->>>>>>> Ticket-06/Create-Hp
-=======
-            <h1>About the team members in steam-wreck</h1>
-            <PeopleCards/>
->>>>>>> 8b672f336e6e954dbe1f32cc7b3779d39ac5c65b
+                <h1>About the team members in steam-wreck</h1>
+                <PeopleCards/>
         </>
     )
 }

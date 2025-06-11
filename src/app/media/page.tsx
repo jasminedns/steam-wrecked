444e6213f4646ed5757bfc2f524cb8544d6002c6
Mustafa Larhnimi <mustafa.larhnimi@gmail.com>
2025-06-11 22:15:49 +0200

    Media Youtube Section (Also remade media page Background)

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,9 +1,10 @@
 import BackgroundWrapper from "@/components/AGBackground"
-
+import YouTubeSection from "@/components/YouTubeSection"
 const MediaPage = () => {
     return (
         <BackgroundWrapper>
             <h1>Media of steam-wreck</h1>
+            <YouTubeSection />
         </BackgroundWrapper>
 
     )
--- src/components/YouTubeSection/index.tsx
+++ src/components/YouTubeSection/index.tsx
@@ -1,10 +1,16 @@
 const YouTubeSection = () => {
     return (
         <div className="w-[70vw] mx-auto bg-transparent">
+            <h2 className=" md:text-5xl charter text-center p-4 whitespace-nowrap text-lg">
+                WATCH TUTORIAL
+            </h2>
             <iframe 
-                className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] border-4"
+                className="w-full border-4 aspect-video"
                 src="https://www.youtube.com/embed/dQw4w9WgXcQ"
                 title="YouTube video"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture web-share"
+                max-width="100%"
+                max-height="100%"
                 allowFullScreen
             ></iframe>
         </div>

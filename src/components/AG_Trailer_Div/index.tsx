964e4f050ff4fcc0618452c7534ef6b27dba39c3
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-11 21:44:37 +0200

    adding trailer div with some youtube video

--- src/components/AG_Trailer_Div/index.tsx
+++ src/components/AG_Trailer_Div/index.tsx
@@ -1,39 +1,32 @@
 'use client'
-import YouTube from 'react-youtube'
 import { useState } from 'react';
-import 'react-social-icons/youtube';
-import { SocialIcon } from 'react-social-icons';
+import { YoutubeLogoIcon } from '@phosphor-icons/react';
+
 const AG_Trailer_Div = () => {
     const [isPlaying, setIsPlaying] = useState(false);
 
-  const videoId = 'dQw4w9WgXcQ'; 
+  const videoId = 'GyO1MtLhyt0'; 
     return (
         <>
-            <div className="flex flex-col m-8">
+            <div className="flex flex-col m-4 md:m-8">
                 <p className="uppercase text-3xl text-left charter mx-6 text-[#F5F5F5]">Trailer</p>
-                <div className="w-full max-w-[450px] m-6 aspect-video relative">
-      {isPlaying ? (
-        <YouTube
-          videoId={videoId}
-          opts={{
-            width: '100%',
-            height: '100%',
-            playerVars: {
-              autoplay: 1,
-            },
-          }}
-          className="w-full h-full"
-        />
-      ) : (
-        <div
-          className="w-full h-full bg-black flex items-center justify-center cursor-pointer"
-          onClick={() => setIsPlaying(true)}
-        >
-          <SocialIcon url='www.youtube.com'  />
-        </div>
-      )}
-    </div>
-            </div>
+                  <div className="relative w-full max-w-[300px] md:max-w-[500px] m-4 md:m-6 aspect-video rounded-lg overflow-hidden">
+                    {isPlaying ? (
+                        <iframe
+                          className="w-full h-full"
+                          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                          title="YouTube Video"
+                          allow="autoplay; encrypted-media"
+                          allowFullScreen
+                        />
+                        ) : (
+                        <div className="w-full h-full bg-black flex items-center justify-center cursor-pointer" onClick={() => setIsPlaying(true)}>
+                            <YoutubeLogoIcon fill='red' size={56}/>
+                        </div>
+                    )}
+                </div>
+                
+            </div>   
         </>
     )
 }

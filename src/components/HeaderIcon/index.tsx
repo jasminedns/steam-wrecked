1bef0daafbc87d3b88670841ffcb2aed47ec1689
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-11 22:12:22 +0200

    ✅ Update HeaderLinks component and related files for Ticket-02-2

--- src/components/HeaderIcon/index.tsx
+++ src/components/HeaderIcon/index.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import HeaderLinks from '../HeaderLinks';
+
 
 const HeaderIcon = () => {
   return (
--- src/components/HeaderLinks/index.tsx
+++ src/components/HeaderLinks/index.tsx
@@ -1,10 +1,12 @@
 
 import Link from 'next/link';
 import React from 'react';
+import HeaderIcon from '../HeaderIcon';
+
 
 const HeaderLinks = () => {
   return (
-    <nav className="flex space-x-6 text-[#E3E3E3] font-charter">
+    <nav className="flex space-x-6 text-[#141010] font-charter justify-center">
       <Link href="/media" className="hover:text-[#FFE066]">MEDIA</Link>
       <Link href="/team" className="hover:text-[#FFE066]">THE TEAM</Link>
       <Link href="/game" className="hover:text-[#FFE066]">GAME</Link>

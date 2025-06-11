e153a0a4440d2ae386ae7edc54decfca2dee1eeb
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-11 14:41:13 +0200

    adding AG PPV div

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,5 @@
 import AGBalloonDiv from "@/components/AG_Balloon_content"
+import AG_PPV_Div from "@/components/AG_PPV_Div"
 import AGHeading from "@/components/AGHeading"
 
 const GamePage = () => {
@@ -7,7 +8,8 @@ const GamePage = () => {
         <div className="relative h-screen bg-[#2F5E72]">
             <div className="absolute inset-0">
                 <div className="relative z-10 text-white flex flex-col items-center justify-center h-full ">
-                    <img src="/images/background/AG_bg.JPEG" alt="Background" className="w-full h-full object-cover object-left-bottom opacity-20 mix-blend-multiply" />
+                    <img src="/images/background/AG_bg.JPEG" alt="Background" className="w-full h-full
+                     object-cover object-left-bottom opacity-20 mix-blend-multiply" />
                     <div className="absolute top-4">
                     <AGHeading />
                     <AGBalloonDiv />
@@ -16,7 +18,10 @@ const GamePage = () => {
             </div>
         </div>
         <div className="bg-[linear-gradient(75deg,rgba(23,109,130,1)_0%,rgba(130,100,75,1)_27%,rgba(32,113,135,1)_61%,rgba(16,84,94,1)_100%)] h-200 ">
-              images and video
+              <div className="flex flex-col md:flex-row justify-around">
+              <AG_PPV_Div />
+              </div>
+              
         </div>
 
         </>

89fab7caf6d794542365edc12908573570aba408
andre84o <andre.torabpour@gmail.com>
2025-06-11 15:03:46 +0200

    changed back codes to ticket-06-5/Clickable-Img-And-Text

--- src/components/ImageLink/index.tsx
+++ src/components/ImageLink/index.tsx
@@ -6,28 +6,26 @@ import "@/app/globals.css"
 const ImageLink = () => {
     const diffImg = (index:number) => {
         if (index === 0) {
-            return <Image src={"/images/player/selected_player.png"} alt="player image" width={200} height={300} className="cursor-pointer w-full h-auto max-w-[120px] sm:max-w-[160px] md:max-w-[200px] lg:max-w-[240px] xl:max-w-[280px]"/>
+            return <Image src={"/images/player/selected_player.png"} alt="player image" width={200} height={300} className="cursor-pointer"/>
         } else if (index === 1) {
-            return <Image src={"/images/player/player1.png"} alt="player image" width={200} height={300} className="cursor-pointer w-full h-auto max-w-[120px] sm:max-w-[160px] md:max-w-[200px] lg:max-w-[240px] xl:max-w-[280px]"/>
+            return <Image src={"/images/player/player1.png"} alt="player image" width={200} height={300} className="cursor-pointer"/>
         } else {
-            return <Image src={"/images/player/player4.png"} alt="player image" width={200} height={300} className="cursor-pointer w-full h-auto max-w-[120px] sm:max-w-[160px] md:max-w-[200px] lg:max-w-[240px] xl:max-w-[280px]"/>
+            return <Image src={"/images/player/player4.png"} alt="player image" width={200} height={300} className="cursor-pointer"/>
         }
     }
 
     return (
         <>
-        <div className="flex flex-wrap justify-center gap-4 px-4 w-full max-w-screen z-20">
             {Pages.map((item:string, index:number) => (
-                <div key={index} className="flex flex-col items-center w-full max-w-[120px] sm:max-w-[160px] md:max-w-[212px] lg:max-w-[260px] xl:max-w-[300px]">
-                    <Link key={index} href={`/${item.replace(/^the /i, "")}`} className="flex flex-col items-center justify-center w-full">
-                        <div className="image__container relative w-full aspect-[2/3]">
+                <div key={index} className="mb-3">
+                    <Link key={index} href={`/${item.replace(/^the /i, "")}`} className="flex flex-col items-center justify-center w-[212px]">
+                        <div className="image__container">
                             {diffImg(index)}
                         </div>
                         {item.includes("the") ? <p className="image__link">about {item}</p> : <p className="image__link">{item}</p>}
                     </Link>
                 </div>
             ))}
-            </div>
         </>
     )
 }

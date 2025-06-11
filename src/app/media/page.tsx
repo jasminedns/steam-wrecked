74a0a9fe73c78f2e6a3151eb85b4dd3b42b4391e
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-11 12:47:24 +0200

    rendering arrow in game and media page

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,6 +1,11 @@
+import BottomToTopArrow from "@/components/BottomToTopArrow"
+
 const MediaPage = () => {
     return (
+        <>
         <h1>Media of steam-wreck</h1>
+        <BottomToTopArrow />
+        </>
     )
 }
 

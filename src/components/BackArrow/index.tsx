e2093bf1862d515f761ae35d2b4bcf810a71718f
andre84o <andre.torabpour@gmail.com>
2025-06-12 14:00:09 +0200

    change dessign

--- src/components/BackArrow/index.tsx
+++ src/components/BackArrow/index.tsx
@@ -1,9 +1,16 @@
+'use client';
+import { useRouter } from "next/navigation";
 import { IoArrowBack } from "react-icons/io5";
 
 const BackArrow = () => {
+    const router = useRouter ();
+
     return (
-        <IoArrowBack color="white" className="text-[24px] sm:text-[32px] md:text-[40px] lg:text-[48px]"/>
-    )
-}
+        <div onClick={() => router.back()}
+      className="cursor-pointer inline-flex items-center justify-center p-0 ml-4 mt-2">
+        <IoArrowBack color="white" className="text-[24px] sm:text-[32px] md:text-[40px] lg:text-[48px] hover:bg-black/50 rounded-full cursor-pointer z-50"/> 
+    </div>
+    );
+};
 
 export default BackArrow

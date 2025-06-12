fd7285ac3bc798b2f1e98e71d539672a71d0df70
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-12 12:11:51 +0200

    centering video

--- src/components/AG_Trailer_Div/index.tsx
+++ src/components/AG_Trailer_Div/index.tsx
@@ -8,9 +8,9 @@ const AG_Trailer_Div = () => {
   const videoId = 'GyO1MtLhyt0'; 
     return (
         <>
-            <div className="flex flex-col m-4 md:m-8">
-                <p className="uppercase text-3xl text-left charter mx-6 text-[#F5F5F5]">Trailer</p>
-                  <div className="relative w-full max-w-[300px] md:max-w-[500px] m-4 md:m-6 aspect-video rounded-lg overflow-hidden">
+            <div className="flex flex-col m-4 md:m-8 md:basis-[50%]">
+                <p className="uppercase text-3xl text-center md:text-left charter mx-6 text-[#F5F5F5]">Trailer</p>
+                  <div className="relative w-full max-w-[300px] md:max-w-[500px] self-center md:self-start m-4 md:m-6 aspect-video rounded-lg overflow-hidden">
                     {isPlaying ? (
                         <iframe
                           className="w-full h-full"

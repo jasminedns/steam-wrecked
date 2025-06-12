28499a29dfb305e7232a9b3851aa8d06b10a67eb
Jasminedns <jasminedanese@icloud.com>
2025-06-12 14:40:48 +0200

    fixed prev merg issue

--- src/components/AG_Balloon_content/index.tsx
+++ src/components/AG_Balloon_content/index.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
 import AGDescriptionDiv from "../AGDescriptionDiv";
-import AGDescriptionDiv from "../AGDescriptionDiv";
 
 const AGBalloonDiv = () => {
     return (
         <>
-
-        <div className="mt-55 relative flex w-full justify-center">
-        <div className="mt-40 relative flex w-full justify-center">
             <AGDescriptionDiv />
-        </div> 
             <Image src="/images/background/airship_concept_art.png" alt="airship" width={0} height={0} sizes="100vw"
                 className="w-[150px] sm:w-[250px] md:w-[200px] lg:w-[300px] h-auto absolute left-[2%]  top-[-200px] sm:top-[-220px] sm:left-[-2%] md:top-[-200px] md:left-[-15%] lg:left-[-45%]  lg:top-[-250px]">
             </Image>
-    
+            
         </>
     )
 }

cfd88d7280afed666e2e818f692b06b78b70b39a
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-14 00:09:21 +0200

    Apply custom font styles globally (Ticket-05-1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-montserrat bg-black text-white">
-        <Header/>
+      <body>
+        <Header />
         {children}
         <Footer />
       </body>

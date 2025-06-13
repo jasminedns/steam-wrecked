b23609cbead2dc639f1eda920e483bebc5e11e30
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-06-13 10:22:36 +0200

    quick fix button

--- src/components/Download/HpDownloadButton.tsx
+++ src/components/Download/HpDownloadButton.tsx
@@ -3,14 +3,18 @@ import React from "react";
 const HpDownloadButton = () => {
   return (
     <button
-      className="w-full max-w-[260px] sm:max-w-[280px] md:max-w-[300px]
-        mt-4 rounded-full px-5 py-2.5 
-        text-white text-lg sm:text-xl font-semibold tracking-wide 
-        cursor-pointer shadow-md 
+      className="
+        w-full max-w-[460px] sm:max-w-[500px] md:max-w-[550px]
+        mt-4
+        px-8 py-3
+        rounded-full
+        text-white text-xl sm:text-2xl font-semibold
         bg-[radial-gradient(circle,_#E67716_0%,_#761E1C_100%)]
-        transition-all duration-300 ease-in-out
-        hover:scale-105 hover:opacity-90 
-        hover:shadow-[0_0_10px_3px_rgba(255,140,0,0.4)]"
+        shadow-md
+        transition duration-300 ease-in-out
+        hover:scale-105 hover:opacity-90 hover:shadow-[0_0_10px_3px_rgba(255,140,0,0.4)]
+        cursor-pointer
+      "
     >
       DOWNLOAD
     </button>

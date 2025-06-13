db59d0924cc42451b77834335de092877e02c3e1
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-06-13 16:27:23 +0200

    fix: color correction

--- src/components/MediaBlogSection/BlogCard.tsx
+++ src/components/MediaBlogSection/BlogCard.tsx
@@ -1,17 +1,17 @@
 import ReadMoreButton from "./ReadMoreButton";
 
-
 const BlogCard = ({
-    title,
-    text,
-    link,
-  }: {
-    title: string;
-    text: string;
-    link: string;
-  }) => {
-    return (
-      <div className="flex flex-col justify-between items-center text-center px-8 py-12 bg-[rgba(125,125,125,0.38)] border-[#0F1D21] border-[12px] rounded-[2rem] text-white w-full max-w-[450px] shadow-lg">
+  title,
+  text,
+  link,
+}: {
+  title: string;
+  text: string;
+  link: string;
+}) => {
+  return (
+    <div className="bg-[#0F1D21] p-[12px] rounded-[2rem] w-full max-w-[450px]">
+      <div className="bg-[rgba(125,125,125,0.38)] rounded-[1.5rem] px-8 py-12 text-white text-center flex flex-col justify-between items-center h-full">
         <h3 className="text-2xl sm:text-3xl font-bold leading-snug uppercase mb-6 font-[charter]">
           {title}
         </h3>
@@ -22,7 +22,9 @@ const BlogCard = ({
           <ReadMoreButton href={link} />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
+
 
   export default BlogCard

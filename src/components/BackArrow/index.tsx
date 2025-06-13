e7704c307fe1a6d56f5af66ffd445c840ac5d25c
andre84o <andre.torabpour@gmail.com>
2025-06-13 12:39:00 +0200

    change size

--- src/components/BackArrow/index.tsx
+++ src/components/BackArrow/index.tsx
@@ -6,9 +6,8 @@ const BackArrow = () => {
     const router = useRouter ();
 
     return (
-        <div onClick={() => router.back()}
-      className="cursor-pointer inline-flex items-center justify-center p-0 ml-4 mt-2">
-        <IoArrowBack color="white" className="text-[24px] sm:text-[32px] md:text-[32px] lg:text-[32px] hover:bg-black rounded-full cursor-pointer z-50"/> 
+        <div className="absolute items-center justify-center hover:bg-black rounded-full ml-4 mt-2 z-50">
+        <IoArrowBack color="white" onClick={router.back} size={32}/> 
     </div>
     );
 };

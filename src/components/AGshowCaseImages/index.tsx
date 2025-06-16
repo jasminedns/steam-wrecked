9ae2e0d75df90c8e8713d691a056a797c4aecc4f
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-16 22:07:10 +0200

    fixing margin

--- src/components/AGshowCaseImages/index.tsx
+++ src/components/AGshowCaseImages/index.tsx
@@ -10,7 +10,7 @@ const showcaseImages:string[] = [
 const AGShowcase = () => {
   return (
     <>
-      <div className='m-10 lg:m-8 py-8 mb-0 flex flex-col lg:flex-row flex-wrap'>
+      <div className='m-10 mb-0 md:px-0 lg:m-8 lg:mb-0 flex flex-col lg:flex-row flex-wrap'>
         {
           showcaseImages.map((item: string, index: number) => (
             <div key={index} className='basis-[50%] justify-items-center'>
@@ -20,7 +20,7 @@ const AGShowcase = () => {
                 width={0} 
                 height={0} 
                 sizes="100vw" 
-                className="mb-[48px] object-contain w-[100%] md:w-[75%] h-auto border-2 border-black" 
+                className="mb-[80px] object-contain w-[100%] md:w-[75%] h-auto border-2 border-black" 
               />
             </div>
           ))

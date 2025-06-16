86ed29ebf7fcb5a8a98363e79da92b48648db088
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-16 16:15:52 +0200

    fixing img responsiveness

--- src/components/AG_PPV_Div/index.tsx
+++ src/components/AG_PPV_Div/index.tsx
@@ -3,7 +3,7 @@ const AG_PPV_Div = () => {
         <>
         <div className="flex flex-col m-4  lg:basis-[50%] lg:m-8 items-center">
             <p className="uppercase text-[24px] md:text-[32px] text-[#F5F5F5] charter m-6 mb-0 text-center">Players point of view</p>
-            <div className="bg-[#060A0A]/65 text-[#F5F5F5]  m-1  max-w-[500px]  lg:mx-0 sm:m-6 rounded-lg">
+            <div className="bg-[#060A0A]/65 text-[#F5F5F5]  m-1 mt-4 max-w-[500px]  lg:mx-0 sm:m-6 rounded-lg">
                 <p className="p-[24px] m-4 sm:m-0 lg:p-[46px] text-xl charter ">You are a scavenger of the Sandwalkers Guild, tasked with retrieving lost resources from the ruins below. 
                     After a crash leaves you stranded in the wasteland, survival becomes your only mission. 
                     Can you repair your ship and return to the skies — or will the dead world claim you?</p>
--- src/components/AGshowCaseImages/index.tsx
+++ src/components/AGshowCaseImages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 
 const showcaseImages:string[] = [
   '/images/background/ScreenShot00012.png',
@@ -8,32 +7,10 @@ const showcaseImages:string[] = [
   '/images/background/ScreenShot00015.png',
 ];
 
-// const AGShowcase = () => {
-//   return (
-//     <section className="w-full flex px-6 py-10 justify-center">
-//       <div className=" m-8 ml-12 px-6 grid grid-cols-1 md:grid-cols-2 items-center">
-//         {showcaseImages.map((src:string, index:number) => (
-//           <div key={index} className='justify-center'>
-//             <Image
-//               src={src}
-//               alt={`Showcase image ${index + 1}`}
-//               width={0} height={0} sizes='100vw'
-//               className="ml-10 my-4 object-contain w-[75%] h-auto"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default AGShowcase;
-
-
 const AGShowcase = () => {
   return (
     <>
-      <div className='m-8 py-8 mb-0 flex flex-col lg:flex-row flex-wrap'>
+      <div className='m-10 lg:m-8 py-8 mb-0 flex flex-col lg:flex-row flex-wrap'>
         {
           showcaseImages.map((item: string, index: number) => (
             <div key={index} className='basis-[50%] justify-items-center'>

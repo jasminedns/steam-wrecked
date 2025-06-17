f567aa003460d6916552ac09a17b21f56e356254
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-17 12:40:52 +0200

    fixing margin

--- src/components/AG_Backstory_Div/index.tsx
+++ src/components/AG_Backstory_Div/index.tsx
@@ -1,6 +1,6 @@
 const AG_Backstory_Div = () => {
     return(
-        <div className="flex flex-col m-6 sm:px-[48px] md:px-0 lg:basis-[50%] lg:m-8 items-center">
+        <div className="flex flex-col m-6 lg:m-8 sm:px-[48px] md:px-0 lg:basis-[50%] items-center">
             <p className="uppercase text-[24px] md:text-[32px] text-[#F5F5F5] charter m-6 mb-0 text-center">Backstory</p>
             <div className="bg-[#060A0A]/65 text-[#F5F5F5]  m-1 mt-4 sm:m-6  md:mx-20  lg:mx-0  max-w-[500px]  md:max-w-fit lg:max-w-[500px] rounded-lg">
                 <p className="p-[24px] m-4 sm:m-0 lg:p-[46px] text-[16px] md:text-xl charter ">

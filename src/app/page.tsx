6045a02185b3b813786b90113a097c1b15ea55d3
Jasminedns <jasminedanese@icloud.com>
2025-06-17 13:16:17 +0200

    got rid of top bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
         <div className="relative h-[70vh] max-sm:h-[50vh] max-md:h-[60vh] bg-[url('/images/background/keyart.jpeg')] bg-cover bg-[center_right_20%] sm:bg-right md:bg-center">
           <HpDownloadDiv/>
         </div>
-        <div className="absolute -mt-3 w-full h-3 bg-gradient-to-b from-transparent to-black/10 backdrop-blur-sm pointer-events-none z-20"></div>
         <div className="relative min-h-[70vh] max-md:min-h-[60vh] max-sm:min-h-[65vh] -mb-26 w-full bg-[url('/images/background/EnvironmentConceptGroundlevel.jpeg')] bg-cover bg-[center_right_20%] sm:bg-right md:bg-center pb-24">
           <div className="absolute top-[-20px] inset-0 bg-gradient-to-b from-[#42a09710] via-transparent to-[#42a09710] z-0 pointer-events-none p-4 bg-[#42a09710] backdrop-blur-sm" ></div>
           <div className="relative z-10 flex justify-evenly flex-wrap md:absolute md:inset-0 md:items-center md:mb-[48px]">

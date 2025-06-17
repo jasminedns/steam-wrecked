0894bc717993b0486e6703e9b1cfd7d28c8c645d
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-17 14:35:31 +0200

    fixing padding

--- src/components/AG_PPV_Div/index.tsx
+++ src/components/AG_PPV_Div/index.tsx
@@ -2,7 +2,7 @@ const AG_PPV_Div = () => {
     return (
         <>
 
-        <div className="flex flex-col  lg:basis-[50%] lg:m-4 items-center">
+        <div className="flex flex-col m-6 lg:basis-[50%] lg:m-4 items-center">
             <p className="uppercase text-[24px] md:text-[32px] text-[#F5F5F5] charter m-6 mb-0 text-center">Players point of view</p>
             <div className="bg-[#060A0A]/65 text-[#F5F5F5]  m-1 mt-4 sm:m-6  md:mx-20  lg:mx-0  max-w-[500px]  md:max-w-fit lg:max-w-[500px] rounded-lg">
                 <p className="p-[8px] m-4 sm:m-0 md:p-[24px] lg:p-[46px] text-[16px] md:text-xl charter ">You are a scavenger of the Sandwalkers Guild, tasked with retrieving lost resources from the ruins below. 

f8a7d05dc30d063f877d9a8dfe8e4adabd190414
andre84o <andre.torabpour@gmail.com>
2025-06-18 15:44:04 +0200

    fixed logi in large screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <header>
       <div className="bg-[#282523] h-14 w-full flex justify-between relative z-99">
-        <HeaderIcon linkClass="flex items-center cursor-pointer basis-[50%]" 
-          imgClass="m-1 sm:m-2 md:m-3  w-[30%] min-[425px]:w-[22%] min-[620px]:w-[20%] min-[700px]:w-[15%] min-[970px]:w-[10%] lg:w-[9%] xl:w-[8%] h-auto"/>         
+        <HeaderIcon linkClass="flex items-center cursor-pointer basis-[50%]"
+          imgClass="m-1 sm:m-2 md:m-3  w-[30%] min-[425px]:w-[22%] min-[620px]:w-[20%] min-[700px]:w-[15%] min-[970px]:w-[10%] lg:w-[9%] xl:w-[7%] h-auto"/>         
         <HeaderLinks />
         <div className="flex justify-end md:hidden z-40">
           <HamburgerMobile />
